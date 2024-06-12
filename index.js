f2aa8468a312e204aa7f2178ba2055783b8a4147
brianpocasangre <brianpocasangre1@gmail.com>
2024-06-12 15:45:12 -0700

    complete axios request for random secret

--- index.js
+++ index.js
@@ -6,14 +6,24 @@ import axios from 'axios';
 // 2. Create an express app and set the port number.
 const app = express();
 const port = 3000;
+const API_URL = 'https://secrets-api.appbrewery.com/random';
 
 // 3. Use the public folder for static files.
 app.use(express.static('public'));
 
 // 4. When the user goes to the home page it should render the index.ejs file.
 app.get('/', async (req, res) => {
-  const result = await axios.get();
-  res.render('index.ejs');
+  try {
+    const response = await axios.get(API_URL);
+    const result = response.data;
+    res.render('index.ejs', {
+      secret: result.secret,
+      user: result.username,
+    });
+  } catch (error) {
+    console.log(error.response.data);
+    res.status(500);
+  }
 });
 // 5. Use axios to get a random secret and pass it to index.ejs to display the
 // secret and the username of the secret.
